2bdb4f56ec7508d9751437eff3d3fff7bdd799f8
Bakhtiyar <Bakhtiyar@DESKTOP-JPQ614N>
2024-01-22 12:31:09 +0600

    Add project files.

---
+++ Snake/ViewModels/MainWndVM.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.ViewModels
+{
+    internal class MainWndVM : BindableBase
+    {
+        private bool _continueGame;
+
+        public bool ContinueGame
+        {
+            get { return _continueGame; }
+            private set
+            {
+                _continueGame = value;
+                RaisePropertyChanged(nameof(ContinueGame));
+            }
+        }
+
+        public DelegateCommand StartStopCommand { get; }
+
+        public MainWndVM()
+        {
+            StartStopCommand = new DelegateCommand(() => ContinueGame = !ContinueGame);
+        }
+
+    }
+}
